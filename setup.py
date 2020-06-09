eba14c4f832f54e29d6a29ce9b7e14cef8562369
Arnaud Marchand <amarchand@icloud.com>
2020-06-09 10:26:48 +0200

    Save point

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name = 'elastic_helper',
   packages = ['elastic_helper'], # this must be the same as the name above
-  version = '1.2.2',
+  version = '1.2.3',
   description = 'Elastic Search to Pandas Dataframe',
   long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'snuids',
   author_email = 'snuids@mannekentech.com',
   url = 'https://github.com/snuids/elastic_helper', 
-  download_url = 'https://github.com/snuids/elastic_helper/archive/1.2.2.tar.gz',
+  download_url = 'https://github.com/snuids/elastic_helper/archive/1.2.3.tar.gz',
   keywords = ['ElasticSearch', 'pandas', 'convert'], # arbitrary keywords
   classifiers = [],
 )
