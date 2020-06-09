35f39651bad1ddf4002772d51d99e3dfad03bd94
Arnaud Marchand <amarchand@icloud.com>
2020-06-09 10:26:30 +0200

    Save point

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
-setup(
+#from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name = 'elastic_helper',
   packages = ['elastic_helper'], # this must be the same as the name above
   version = '1.2.2',
   description = 'Elastic Search to Pandas Dataframe',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'snuids',
   author_email = 'snuids@mannekentech.com',
   url = 'https://github.com/snuids/elastic_helper', 
